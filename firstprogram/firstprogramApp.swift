ab7f27b51b217bfa966585f59d039fb4dcbe68da
Syed Raza <syed.raza011@gmail.com>
2023-06-09 20:30:53 -0400

    Initial Commit

---
+++ firstprogram/firstprogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstprogramApp.swift
+//  firstprogram
+//
+//  Created by Syed Raza on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct firstprogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
